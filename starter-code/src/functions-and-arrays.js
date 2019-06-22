ab490d178dcef5952fe459ae2c270bc97c15615d
Victor Thoma <victor@mrundmrsthoma.de>
2019-06-22 18:05:41 +0200

    some changhes

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -154,7 +154,7 @@ function howManyTimes(searchFieldCount, searchWordCount) {
   }
   return count;
 }
-// Bonus Quest
+// Bonus Questkjkj
 
 var matrix = [
   [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
